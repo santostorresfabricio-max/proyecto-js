36c095b3cfbbe2a75d92e7fd9968073e6e655355
Josue Rios <fernanflo.hptmail@gmail.com>
2026-03-12 15:22:12 -0500

    Fix console log statement for user information
    me falto poner un punto y coma a la linea 5

--- actividad2.js
+++ actividad2.js
@@ -2,4 +2,4 @@ let nombre = prompt("Ingrese su nombre");
 let edad = prompt("Ingrese su edad");
 
 alert("Bienvenido " + nombre );
-console.log("El usuario "+ nombre + " tiene " +edad+" años")
+console.log("El usuario "+ nombre + " tiene " +edad+" años");
