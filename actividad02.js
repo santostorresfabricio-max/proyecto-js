54c8c8df652e174aefc1b5758a104d7960742e3f
Fabricio Santos <santostorresfabricio@gmail.com>
2026-04-08 02:34:29 -0500

    Cambios realizados.

---
+++ actividad02.js
@@ -0,0 +1,9 @@
+let nota = prompt("Ingresa tu nota final:");
+
+nota = parseInt(nota);
+
+if (nota >= 13) {
+    console.log("¡Aprobado!");
+} else {
+    console.log("Desaprobado");
+}
---
+++ actividad2.js
@@ -0,0 +1,5 @@
+let nombre = prompt("Ingrese su nombre");
+let edad = prompt("Ingrese su edad");
+
+alert("Bienvenido " + nombre + " tienes " + edad + " años");
+console.log("El usuario "+ nombre + " tiene " +edad+" años")
